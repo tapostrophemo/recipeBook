fa70b20545f502631baed5012a37db4a0574a5a0
Dan Parks <dan@eastofcleveland.com>
2010-06-25 21:48:00 -0400

    made save/cancel button consistent w/other screens

--- app/views/friends/add.php
+++ app/views/friends/add.php
@@ -11,7 +11,14 @@
   <td><input type="text" name="email"/></td>
  </tr>
  <tr>
-  <td colspan="2"><input type="submit" value="Send Invitation"/></td>
+  <td colspan="2">
+   <input type="submit" value="Send Invitation"/>
+  <?php if ($this->input->is_ajax()): ?>
+   <input type="button" value="Cancel" onclick="$.modal.close()"/>
+  <?php else: ?>
+   <input type="button" value="Cancel" onclick="document.location.href='<?=base_url()?>manage'"/>
+  <?php endif; ?>
+  </td>
  </tr>
 </table>
 </form>
