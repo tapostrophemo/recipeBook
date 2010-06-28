b156338750ad8e1f4bf68a92381424e70fcab30e
Dan Parks <dan@eastofcleveland.com>
2010-06-28 00:08:27 -0400

    what did I miss?

--- app/libraries/MY_Controller.php
+++ app/libraries/MY_Controller.php
@@ -6,18 +6,16 @@ class MY_Controller extends Controller
     Controller::__construct();
 
     $this->load->helper('cookie');
+    $data = array('name' => 'mkt', 'expire' => 90 * 24 * 60 * 60); // expire in 90 days
     if (!$cookie = get_cookie('mkt', true)) {
       $this->load->model('Marketing');
       $row = $this->Marketing->newVisitor($this->input->server('HTTP_REFERER'), current_url());
-      $data = array(
-        'name' => 'mkt',
-        'value' => $row->cookie_id,
-        'expire' => 90 * 24 * 60 * 60); // now (from CI) + 90 days
-      set_cookie($data);
+      $data['value'] = $row->cookie_id;
     }
     else {
-      // TODO: update cookie expiration time, info in DB
+      $data['value'] = $cookie;
     }
+    set_cookie($data);
   }
 }
 
